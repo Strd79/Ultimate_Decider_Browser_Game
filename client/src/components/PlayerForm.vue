993d4345669c926da344a45b406675396f6ab0e0
David Strain <strd79@hotmail.co.uk>
2020-10-23 16:49:46 +0100

    started player form

--- client/src/components/PlayerForm.vue
+++ client/src/components/PlayerForm.vue
@@ -1,5 +1,11 @@
 <template>
-  
+  <div>
+      <h1>Add A Player:</h1>
+      <form >
+        <label for="player">Player name:</label>
+        <input type="text" id="player">
+      </form>
+  </div>
 </template>
 
 <script>
