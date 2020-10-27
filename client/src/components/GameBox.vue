c2ea912443b40440ddaa01298c86ca2a140ad5e2
Campbell Murray <cam@Campbells-MacBook-Air.local>
2020-10-27 17:17:44 +0000

    game randomising

--- client/src/components/GameBox.vue
+++ client/src/components/GameBox.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <match-score-board :playerOne='playerOne' :playerTwo="playerTwo" :playerOneMatchScore="playerOneMatchScore" :playerTwoMatchScore="playerTwoMatchScore" ></match-score-board>
-        <memory-match-game :playerOne='playerOne' :playerTwo="playerTwo" />
-        <rock-paper-scissors-game :playerOne='playerOne' :playerTwo="playerTwo" :playerOneMatchScore="playerOneMatchScore" :playerTwoMatchScore="playerTwoMatchScore" ></rock-paper-scissors-game>
+        <memory-match-game v-if="selectedGame === 0" :playerOne='playerOne' :playerTwo="playerTwo" />
+        <rock-paper-scissors-game v-if="selectedGame === 1" :playerOne='playerOne' :playerTwo="playerTwo" :playerOneMatchScore="playerOneMatchScore" :playerTwoMatchScore="playerTwoMatchScore" ></rock-paper-scissors-game>
          
     </div>
 </template>
@@ -18,6 +18,7 @@ export default {
     props: ['playerOne', 'playerTwo'],
     data(){
         return {
+            selectedGame: null,
             playerOneMatchScore:0,
             playerTwoMatchScore:0
         }
@@ -31,10 +32,19 @@ export default {
              console.log("playerTwoIncrease called");
             this.playerTwoMatchScore += 1
         })
+        eventBus.$on('game-started', () => {
+            this.randomiseGame()
+        })
         
-
-
     },
+
+    methods: {
+        randomiseGame() {
+          this.selectedGame = Math.floor((Math.random() * 2))
+          
+        }
+    },
+
     components: {
         'memory-match-game': MemoryMatchGame,
         'rock-paper-scissors-game': RockPaperScissors,
