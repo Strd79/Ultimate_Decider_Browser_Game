ecebcbac656e0006d268d29751b8148f1ab77644
Rebekah Salem-Dalgety <rebekahsalem-dalgety@Rebekahs-MacBook-Pro.local>
2020-10-28 12:58:07 +0000

    fixed RPS output

--- client/src/components/rps/PlayerRps.vue
+++ client/src/components/rps/PlayerRps.vue
@@ -1,16 +1,6 @@
 <template>
     <div class='playerRps'>
         <h2 v-if="playerOne && !winner && !draw">Player: {{ this.activePlayer.name}}</h2>
-        <div v-if="winner">
-            <h2> {{this.playerOne.name}} picked {{this.playerOne_choice}}.</h2> 
-            <h2> {{this.playerTwo.name}} picked {{this.playerTwo_choice}}.</h2> 
-            <h2 > And the winner is {{this.winner}}!</h2>
-        </div>
-        <div v-if="draw">
-            <h2> {{this.playerOne.name}} picked {{this.playerOne_choice}}.</h2> 
-            <h2> {{this.playerTwo.name}} picked {{this.playerTwo_choice}}.</h2> 
-            <h2 > {{this.draw}} </h2>
-        </div>
         
         <button v-on:click="makeRock">Rock</button>
         <button v-on:click="makePaper">Paper</button>
